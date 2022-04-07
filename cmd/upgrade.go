949ed0120e222f00c9c0dc588bea81fba8e1c430
clibing <liubaixun@yinxiang.com>
2022-04-07 16:58:25 +0800

    feat(upgrade): add bin auto upgrade
    add bin auto upgrade
    
    Signed-off-by: clibing <liubaixun@yinxiang.com>

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -17,16 +17,27 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 var (
+	// 下载地址
+	download = ""
 	// 升级的版本
 	upgrade = ""
 	// 二进制的位置
 	bin = ""
 )
+
+const downloadURL = "https://github.com/clibing/knife/releases/download/%s/knife_%s_%s"
+
 // upgradeCmd represents the upgrade command
 var upgradeCmd = &cobra.Command{
 	Use:   "upgrade",
@@ -36,8 +47,79 @@ var upgradeCmd = &cobra.Command{
 knife upgrade -u "0.0.6" -b /usr/local/bin/
 .`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("upgrade called")
+		if len(download) == 0 && len(upgrade) == 0 {
+			fmt.Println("参数不能为空")
+			return
+		}
+
+		target := ""
+		if len(download) != 0 {
+			target = download
+		} else {
+			arch := runtime.GOARCH
+			os := runtime.GOOS
+			target = fmt.Sprintf(downloadURL, upgrade, os, arch)
+		}
+
+		fmt.Println("下载的地址: ", target)
+
+		file := filepath.Join(path, bin)
+
+		// backup
+		if fileExist(file) {
+			if e :=autoBackup(file); e != nil {
+				fmt.Println(e.Error())
+				return
+			}
+		}
+
+		res, err := http.Get(target)
+		defer func() { _ = res.Close }()
+
+		if err != nil {
+			fmt.Println("下载失败: ", target)
+			return
+		}
+
+		pf, err := os.Create(file)
+		defer func() { _ = pf.Close }()
+		if err != nil {
+			fmt.Println("下载失败: ", target)
+			return
+		}
+
+		io.Copy(pf, res.Body)
+		os.Chmod(file, 0777)
 	},
+}
+
+func autoBackup(file string) (error){
+	bak, err := os.Open(file)
+	if err != nil {
+		fmt.Println("打开现有文件异常", err)
+		return err
+	}
+	defer func() { _ = bak.Close() }()
+
+	suffix := version
+	if len(version) == 0 {
+		suffix = strconv.Itoa(int(time.Now().UnixMilli() / 1000))
+	}
+	bakF := fmt.Sprintf("%s.%s", file, suffix)
+	pp, err := os.Create(bakF)
+	if err != nil {
+		fmt.Println("创建备份异常: ", err)
+		return err
+	}
+	defer func() { _ = pp.Close() }()
+	io.Copy(pp, bak)
+	fmt.Println("备份数据成功, ", bakF)
+	return nil
+}
+
+func fileExist(path string) bool {
+	_, err := os.Lstat(path)
+	return !os.IsNotExist(err)
 }
 
 func init() {
@@ -47,7 +129,10 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// upgradeCmd.PersistentFlags().String("foo", "", "A help for foo")
+	upgradeCmd.PersistentFlags().StringVarP(&download, "download", "d", "", "下载url")
+	upgradeCmd.PersistentFlags().StringVarP(&upgrade, "upgrade", "u", "", "升级的tag，例如v0.0.1")
+	upgradeCmd.PersistentFlags().StringVarP(&path, "path", "p", "/usr/local/bin", "二进制的保存目录, 默认为 /usr/local/bin")
+	upgradeCmd.PersistentFlags().StringVarP(&bin, "bin", "b", "knife", "二进制安装的名字, 默认为 knife")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
