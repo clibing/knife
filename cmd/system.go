07f9eacd3bdda2bbabbcb7944e62f8839bf59cfd
clibing <wmsjhappy@gmail.com>
2023-08-31 23:03:52 +0800

    refactor(code): 调整代码结构
    调整代码结构
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

--- cmd/system.go
+++ cmd/system.go
@@ -1,9 +1,73 @@
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	"github.com/knife/cmd/system"
 	"github.com/spf13/cobra"
 )
+
+var path string
+
+// uninstallCmd represents the uninstall command
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "卸载",
+	Long: `将当前可执行的二进制程序，从到系统指定目录下移除，默认目录/usr/local/bin:
+
+1. 默认卸载
+knife uninstall 
+2. 指定目录卸载
+knife uninstall -p /usr/local/bin.`,
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Println("uninstall called")
+	},
+}
+
+// installCmd represents the install command
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "安装",
+	Long: `将当前可执行的二进制程序，安装到系统指定目录下，默认/usr/local/bin:
+
+1. 默认安装
+knife install 
+2. 指定目录安装
+knife install -p /usr/local/bin
+.`,
+	Run: func(_ *cobra.Command, _ []string) {
+		binPath, err := exec.LookPath(os.Args[0])
+		if err != nil {
+			fmt.Printf("failed to get bin file info: %s: %s", os.Args[0], err)
+			return
+		}
+
+		currentFile, err := os.Open(binPath)
+		if err != nil {
+			fmt.Printf("failed to get bin file info: %s: %s", binPath, err)
+			return
+		}
+		defer func() { _ = currentFile.Close() }()
+
+		installFile, err := os.OpenFile(filepath.Join(path, "knife"), os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+		if err != nil {
+			fmt.Printf("failed to create bin file: %s: %s", filepath.Join(path, "knife"), err)
+			return
+		}
+		defer func() { _ = installFile.Close() }()
+
+		_, err = io.Copy(installFile, currentFile)
+		if err != nil {
+			fmt.Printf("failed to copy file: %s: %s", filepath.Join(path, "knife"), err)
+			return
+		}
+		fmt.Println("install success")
+	},
+}
 
 var systemCmd = &cobra.Command{
 	Use:     "system",
@@ -16,7 +80,13 @@ var systemCmd = &cobra.Command{
 
 func init() {
 	systemCmd.AddCommand(system.NewArchCmd(), system.NewMonitorCmd(), system.NewUpgradeCmd())
-
 	// 转换器
 	rootCmd.AddCommand(systemCmd)
+
+	// 安装 cmd
+	installCmd.PersistentFlags().StringVarP(&path, "path", "p", "/usr/local/bin", "安装目录，window需要指定目录")
+	rootCmd.AddCommand(installCmd)
+
+	// 下载cmd
+	rootCmd.AddCommand(uninstallCmd)
 }
