cb980d3bd64ad8cfd284551cf5c187763ef06ad9
clibing <wmsjhappy@gmail.com>
2023-08-31 22:59:58 +0800

    fix(sign): add sign method
    add sign method
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

---
+++ cmd/sign/hmac.go
@@ -0,0 +1,4 @@
+package sign
+
+// https://zhuanlan.zhihu.com/p/347114235
+// HmacMD5/HmacSHA1/HmacSHA256/HmacSHA384/HmacSHA512
---
+++ cmd/sign/sign.md
@@ -0,0 +1,4 @@
+参考：https://baijiahao.baidu.com/s?id=1607127562017641658&wfr=spider&for=pco
+
+怎么下载 https 网站的 公钥证书呢
+
