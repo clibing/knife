f2eed64a69846c76a772a13871aff9c5beba05d3
clibing <wmsjhappy@gmail.com>
2023-09-01 11:30:08 +0800

    style(format): format
    format
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

--- cmd/sign.go
+++ cmd/sign.go
@@ -14,7 +14,12 @@ var signCmd = &cobra.Command{
 }
 
 func init() {
-	signCmd.AddCommand(sign.NewRsaCmd(), sign.NewMd5Cmd(), sign.NewShaCmd(), sign.NewBase64Cmd())
+	signCmd.AddCommand(
+		sign.NewRsaCmd(),
+		sign.NewMd5Cmd(),
+		sign.NewShaCmd(),
+		sign.NewBase64Cmd(),
+	)
 
 	// 转换器
 	rootCmd.AddCommand(signCmd)
