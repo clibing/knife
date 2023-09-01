58cb24e033a85409d06b82a7d1243df36fd75f00
clibing <wmsjhappy@gmail.com>
2023-09-01 11:18:41 +0800

    fix(code): delete useless code
    delete useless code
    
    Signed-off-by: clibing <wmsjhappy@gmail.com>

--- cmd/sign.go
+++ cmd/sign.go
@@ -14,7 +14,7 @@ var signCmd = &cobra.Command{
 }
 
 func init() {
-	signCmd.AddCommand(sign.NewRsaCmd(), sign.NewMd5Cmd(), sign.NewShaCmd(), sign.NewBase64Cmd(), sign.NewSignCmd())
+	signCmd.AddCommand(sign.NewRsaCmd(), sign.NewMd5Cmd(), sign.NewShaCmd(), sign.NewBase64Cmd())
 
 	// 转换器
 	rootCmd.AddCommand(signCmd)
