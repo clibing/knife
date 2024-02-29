4f902e6724dd67146d5ef2bcc63ae757b9d37b9b
clibing <liubaixun@yinxiang.com>
2024-02-29 18:56:46 +0800

    fix(beaufity): 美化值
    美化值
    
    Signed-off-by: clibing <liubaixun@yinxiang.com>

--- cmd/system/beautify.go
+++ cmd/system/beautify.go
@@ -1,6 +1,9 @@
 package system
 
 import (
+	"fmt"
+
+	"github.com/clibing/knife/internal/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -8,13 +11,21 @@ import (
 var beautifyCmd = &cobra.Command{
 	Use:   "beautify",
 	Short: "字节美化工具, 反向计算字节",
+	Long: `example: 
+
+将字节转换为美化的+单位
+knife system beautify -b 1026
+	
+	`,
 	Run: func(c *cobra.Command, _ []string) {
-		c.Help()
+		size, _ := c.Flags().GetInt64("byte")
+		name := utils.BeautifyValue(size)
+		fmt.Println(name)
 	},
 }
 
 func init() {
-	beautifyCmd.Flags().StringP("byte", "b", "", "")
+	beautifyCmd.Flags().Int64P("byte", "b", 0, "字节")
 }
 
 func NewBeautifyCmd() *cobra.Command {
