b2e3755570f3d0baeca6499392ac7c9d3bb9369c
clibing <liubaixun@yinxiang.com>
2024-03-07 19:05:44 +0800

    fix(value): random value, remove \n
    random value, remove \n
    
    Signed-off-by: clibing <liubaixun@yinxiang.com>

--- cmd/common/random.go
+++ cmd/common/random.go
@@ -42,7 +42,7 @@ var randomCmd = &cobra.Command{
 			for _, value := range input {
 				currentSize := len(value)
 				if inputSize == currentSize {
-					latest := fmt.Sprintf("%s-%s-%s-%s-%s\n", value[0:7], value[8:12], value[12:16], value[16:20], value[20:])
+					latest := fmt.Sprintf("%s-%s-%s-%s-%s", value[0:7], value[8:12], value[12:16], value[16:20], value[20:])
 					fmt.Println(strings.ToUpper(latest))
 					fmt.Println(strings.ToLower(latest))
 				} else if currentSize == formatSize {
